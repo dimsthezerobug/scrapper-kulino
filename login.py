2a60137447e15f0a93d09fbc33bbf50f2fe37e1f
Dimas Saputro <76572359+dimasdoubles@users.noreply.github.com>
2021-08-16 18:51:54 +0700

    Add files via upload
    Program sudah bisa masuk ke dashboard kulino mahasiswa

---
+++ login.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://kulino.dinus.ac.id/"
+
+with open("account.txt") as file:
+    account = file.readline().split()
+
+username = account[0]
+password = account[1]
+session = requests.Session()
+
+
+def login():
+    # masuk kulino
+    respond = session.get(url=URL)
+
+    # cari token
+    soup = BeautifulSoup(respond.text, "html.parser")
+    token = soup.select_one("#block-login > input:nth-child(3)").get("value")
+    # print(token)
+
+    # login ke kulino
+    data = {'anchor': '', 'logintoken': token, 'username': username, 'password': password,}
+    respond_2 = session.post(url=URL + "/login/index.php", data=data)
+
+    # dashboard
+    respond_3 = session.get("https://kulino.dinus.ac.id/my/")
+    dashboard_content = respond_3.text
+    soup_dashboard = BeautifulSoup(dashboard_content, "html.parser")
+    nim_name = (soup_dashboard.select_one(".usertext").string).split(' - ')
+    NAME = nim_name[-1]
+
+    print(f"SELAMAT DATANG DI ABSENNO {NAME}\n")
+
+    return dashboard_content
+
+
+def main():
+    dashboard_content = login()
+
+    # tampilan menu
+    print("Daftar Menu: ")
+    print("\n1. Absensi\n2. Cek Absensi\n3. Pindah Akun\n")
+    order = int(input("Silahkan pilih menu (masukan angka 1/2/3) : "))
+
+
+if __name__ == "__main__":
+    main()
