4599fe9ba1cd9b0138b4a657149f7e5f35adf495
dimassdoubles <codedimas@gmail.com>
2022-02-10 06:25:33 +0700

    berhasil getCourses

--- login.py
+++ login.py
@@ -39,6 +39,18 @@ class Absenno:
 
         return name
 
+    def getCourses(self):
+        dashboard_content = self.getDashboard()
+        soup = BeautifulSoup(dashboard_content, "html.parser")
+
+        courses = {}
+        for course in soup.select(".type_course.depth_3"):
+            course_id = course.find("p")["data-node-key"]
+            course_name = course.find("a")["title"][8:]
+            courses[course_name] = course_id
+
+        return courses
+
 
 def main():
     username = os.environ["username_kul"]
@@ -46,6 +58,7 @@ def main():
 
     absen = Absenno(username, password)
     print(absen.getName())
+    print(absen.getCourses())
 
 
 if __name__ == "__main__":
