c12f705623187ab0d6d22ccef982b21a74a6a15b
dimassdoubles <codedimas@gmail.com>
2022-02-10 09:01:45 +0700

    berhasil absen(sampai mencari pertanyaan forum diskusi)

--- absenno.py
+++ absenno.py
@@ -12,6 +12,7 @@ class Absenno:
         self.password = password
         self.session = requests.Session()
         self.url = "https://kulino.dinus.ac.id/"
+        self.targets = []
         self.login()
 
     def login(self):
@@ -56,7 +57,45 @@ class Absenno:
         for course, id in courses.items():
             print(id, "-", course)
 
+    def setTarget(self, id_course):
+        self.targets.append(id_course)
 
+    def absen(self):
+        for target in self.targets:
+            # masuk ke course
+            respond_course = self.session.get(self.url + "/course/view.php?id=" + target)
+            soup_course = BeautifulSoup(respond_course.text, "html.parser")
+
+            # mencari forum terbaru
+            forums = soup_course.find_all(class_="modtype_forum")
+            if len(forums) > 1:
+                discussion_id = ""
+                forums = forums[1:]
+                i = len(forums) - 1
+                is_continue = True
+                while is_continue:
+                    if i == 0:
+                        is_continue = False
+
+                    forum_id = forums[i]["id"].split("-")[-1]
+
+                    try:
+                        respond_forum = self.session.get(self.url+"/mod/forum/view.php?id=" + forum_id)
+                        soup_forum = BeautifulSoup(respond_forum.text, "html.parser")
+                        discussion_id = soup_forum.select_one("tr.discussion")["data-discussionid"]
+                        is_continue = False
+                    except:
+                        print("[Closed]", forums[i].select_one("span.instancename").text)
+
+                    i -= 1
+
+                if discussion_id != "":
+                    respond_discussion = self.session.get(self.url + "/mod/forum/discuss.php?d=" + discussion_id)
+                    with open("discussion.html", "w", encoding="utf-8") as file:
+                        file.write(respond_discussion.text)
+                    soup_discussion = BeautifulSoup(respond_discussion.text, "html.parser")
+                    question = soup_discussion.find(class_="starter").find("p").text
+                    print("Pertanyaan: " + question)        
 
 
 def main():
@@ -67,6 +106,9 @@ def main():
     print(absen.getName())
     # print(absen.getCourses())
     absen.showCourses()
+    while True:
+        absen.setTarget(input("Masukan kode matkul: "))
+        absen.absen()
 
 
 if __name__ == "__main__":
