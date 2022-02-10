ee25554315c6dc90378ac8c5b47e9500f86fb3cb
dimassdoubles <codedimas@gmail.com>
2022-02-10 11:24:05 +0700

    berhasil absen sesuai jadwal, bisa generate jawaban teman sekelas, tapi belum bisa komentar

--- absenno.py
+++ absenno.py
@@ -1,4 +1,5 @@
 import os
+import random
 import requests
 import warnings
 from bs4 import BeautifulSoup
@@ -8,13 +9,15 @@ warnings.filterwarnings("ignore")
 
 class Absenno:
     def __init__(self, username, password):
+        self.courses = {}
+        self.name = ""
+        self.url = "https://kulino.dinus.ac.id/"
         self.username = username
         self.password = password
         self.session = requests.Session()
-        self.name = ""
-        self.url = "https://kulino.dinus.ac.id/"
         self.targets = []
         self.login()
+        self.getCourses()
 
     def login(self):
         # ke kulino
@@ -42,30 +45,43 @@ class Absenno:
             print(name)
         else:
             print(self.name)
-        
 
     def getCourses(self):
-        dashboard_content = self.getDashboard()
-        soup = BeautifulSoup(dashboard_content, "html.parser")
+        if len(self.courses) == 0:
+            dashboard_content = self.getDashboard()
+            soup = BeautifulSoup(dashboard_content, "html.parser")
 
-        courses = {}
-        for course in soup.select(".type_course.depth_3"):
-            course_id = course.find("p")["data-node-key"]
-            course_name = course.find("a")["title"][8:]
-            courses[course_name] = course_id
+            courses = {}
 
-        return courses
+            for course in soup.select(".type_course.depth_3"):
+                course_id = course.find("p")["data-node-key"]
+                course_name = course.find("a")["title"][8:]
+                courses[course_id] = course_name
+
+            self.courses = courses
+
+        return self.courses
 
     def showCourses(self):
         courses = self.getCourses()
-        for course, id in courses.items():
-            print(id, "-", course)
+        for id, course in courses.items():
+            print("   ", id, "-", course)
 
     def setTarget(self, id_course):
         self.targets.append(id_course)
 
+    def generateAnswer(self, soup_discussion):
+        all_answer = soup_discussion.find_all(class_="text_to_html")
+        answer = all_answer[random.randrange(len(all_answer) + 1)].text
+
+        return answer
+
+    def comment(self, komentar):
+        pass
+
     def absen(self):
         for target in self.targets:
+            print(f"\n[+] ABSEN {self.courses[target]}")
             # masuk ke course
             respond_course = self.session.get(self.url + "/course/view.php?id=" + target)
             soup_course = BeautifulSoup(respond_course.text, "html.parser")
@@ -89,17 +105,21 @@ class Absenno:
                         discussion_id = soup_forum.select_one("tr.discussion")["data-discussionid"]
                         is_continue = False
                     except:
-                        print("[Closed]", forums[i].select_one("span.instancename").text)
+                        # print("    [Closed]", forums[i].select_one("span.instancename").text)
+                        pass
 
                     i -= 1
 
                 if discussion_id != "":
+                    # NOTE : belum ada proses validasi apakah user sudah pernah komentar atau belum
                     respond_discussion = self.session.get(self.url + "/mod/forum/discuss.php?d=" + discussion_id)
-                    with open("discussion.html", "w", encoding="utf-8") as file:
-                        file.write(respond_discussion.text)
                     soup_discussion = BeautifulSoup(respond_discussion.text, "html.parser")
                     question = soup_discussion.find(class_="starter").find("p").text
-                    print("Pertanyaan: " + question)        
+                    answer = self.generateAnswer(soup_discussion)
+                    print("    [?] " + question)
+                    print("    [=]", answer)
+                else:
+                    print("    [!] Tidak Ada Forum Yang Terbuka") 
 
 
 def main():
--- main.py
+++ main.py
@@ -1,71 +1,52 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://kulino.dinus.ac.id/"
-
-with open("account.txt") as file:
-    account = file.readline().split()
-
-username = account[0]
-password = account[1]
-session = requests.Session()
-
-
-def login():
-    # masuk kulino
-    respond = session.get(url=URL)
-
-    # cari token
-    soup = BeautifulSoup(respond.text, "html.parser")
-    token = soup.select_one("#block-login > input:nth-child(3)").get("value")
-    # print(token)
-
-    # login ke kulino
-    data = {'anchor': '', 'logintoken': token, 'username': username, 'password': password,}
-    respond_2 = session.post(url=URL + "/login/index.php", data=data)
-
-    # dashboard
-    respond_3 = session.get("https://kulino.dinus.ac.id/my/")
-    dashboard_content = respond_3.text
-    soup_dashboard = BeautifulSoup(dashboard_content, "html.parser")
-    nim_name = (soup_dashboard.select_one(".usertext").string).split(' - ')
-    NAME = nim_name[-1]
-
-    print(f"SELAMAT DATANG DI ABSENNO {NAME}\n")
-
-    return dashboard_content
-
-
-def change_acct():
-    username = input("\nMasukan username: ")
-    password = input("Masukan password: ")
-    
-    with open("account.txt", mode="w") as file:
-        file.write(f"{username} {password}")
-       
-    print("\nAkun Telah Diganti")
+import os
+from absenno import Absenno
+from datetime import datetime
+from termcolor import colored
 
 
 def main():
-    dashboard_content = login()
 
-    # tampilan menu
-    print("Daftar Menu: ")
-    print("\n1. Absensi\n2. Cek Absensi\n3. Pindah Akun\n")
-    try:
-        order = int(input("Silahkan pilih menu (masukan angka 1/2/3) : "))
-    except:
-        print("Terjadi Kesalahan")
-       
-    if order == 1:
-        pass
-    elif order == 2:
-        pass
-    elif order == 3:
-        change_acct()
-    else:
-        print("Terjadi Kesalahan")
-  
+    USERNAME = os.environ["username_kul"]
+    PASSWORD = os.environ["password_kul"]
+
+    # monday = 0, sunday = 6
+    JADWAL = {  0: ["3481", "3441"],
+                1: ["3374", "3363"],
+                2: ["3326", "3406"],
+                3: ["3282"],
+                4: ["3269"]
+             }
+
+    print("""
+     _      ____    ____    _____   _   _   _   _    ___  
+    / \    | __ )  / ___|  | ____| | \ | | | \ | |  / _ \ 
+   / _ \   |  _ \  \___ \  |  _|   |  \| | |  \| | | | | |
+  / ___ \  | |_) |  ___) | | |___  | |\  | | |\  | | |_| |
+ /_/   \_\ |____/  |____/  |_____| |_| \_| |_| \_|  \___/ 
+                                                          
+""")
+
+    day = datetime.today().weekday()
+    day = 2
+
+    today = Absenno(USERNAME, PASSWORD)
+    print("[+] SELAMAT DATANG ", end="")
+    today.whoami()
+
+    print("\n[+] DAFTAR MATKUL\n")
+    today.showCourses()
+
+    print("\n")
+
+    for id in JADWAL[day]:
+        today.setTarget(id)
+    today.absen()
+
+    is_continue = True
+    while is_continue:
+        input("\n\nKlik Enter untuk Exit")
+        is_continue = False
+
 
 if __name__ == "__main__":
     main()
